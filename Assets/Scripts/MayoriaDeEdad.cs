028ebedc7f048d08450580657851e307380b372f
UrielJolodovsky <46960341@est.ort.edu.ar>
2022-04-06 15:38:03 -0300

    Video 2 finished

---
+++ Assets/Scripts/MayoriaDeEdad.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MayoriaDeEdad : MonoBehaviour
+{
+    public int edadUsuario;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (edadUsuario >=18)
+        {
+            Debug.Log("Es mayor de edad");
+        }
+        else
+        {
+            Debug.Log("No es mayor de edad");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
