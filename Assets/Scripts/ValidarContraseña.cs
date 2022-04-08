7caabdda7103d173a559c0156a890d178980dda4
Uriel Jolodosvky <urieljolo@gmail.com>
2022-04-07 21:34:30 -0300

    Video 6 finished

--- Assets/Scripts/ValidarContraseña.cs
+++ Assets/Scripts/ValidarContraseña.cs
@@ -9,10 +9,13 @@ public class ValidarContraseña : MonoBehaviour
     string contraseniaCorrecta;
     string contraseniaUsuario;
     public Text ingresoUsuario;
+    public Text textoMsj;
+    public GameObject cartelitoMsj;
     // Start is called before the first frame update
     void Start()
     {
         contraseniaCorrecta = "12345";
+        cartelitoMsj.SetActive(false);
     }
 
     // Update is called once per frame
@@ -25,10 +28,14 @@ public class ValidarContraseña : MonoBehaviour
         contraseniaUsuario = ingresoUsuario.text;
         if (contraseniaUsuario == contraseniaCorrecta)
         {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Bienvenido";
             Debug.Log("Bienvenido");
         }
         else
         {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Contraseña Incorrecta";
             Debug.Log("Contraseña incorrecta");
         }
     }
