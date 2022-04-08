fe779c4f64f8a5d7cba706fe7f948539c8766b55
Uriel Jolodosvky <urieljolo@gmail.com>
2022-04-07 21:21:32 -0300

    Video 5 finished

--- Assets/Scripts/ValidarContraseña.cs
+++ Assets/Scripts/ValidarContraseña.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ValidarContraseña : MonoBehaviour
 {
+
+    string contraseniaCorrecta;
+    string contraseniaUsuario;
+    public Text ingresoUsuario;
     // Start is called before the first frame update
     void Start()
     {
-        
+        contraseniaCorrecta = "12345";
     }
 
     // Update is called once per frame
@@ -17,6 +22,14 @@ public class ValidarContraseña : MonoBehaviour
     }
     public void validarContrasenia()
     {
-
+        contraseniaUsuario = ingresoUsuario.text;
+        if (contraseniaUsuario == contraseniaCorrecta)
+        {
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            Debug.Log("Contraseña incorrecta");
+        }
     }
 }
